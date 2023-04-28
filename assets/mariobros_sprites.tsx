5b9dca84ac1d40c014ac47365ae8a17f0115286f
Leonardo Essam <leonardo.deirossi@icloud.com>
2023-04-28 19:42:51 +0200

    Added game assets (sprites)

---
+++ assets/mariobros_sprites.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="mariobros_sprites" tilewidth="16" tileheight="16" spacing="2" margin="1" tilecount="1554" columns="37">
+ <image source="mariobros_sprites.png" width="680" height="764"/>
+</tileset>
